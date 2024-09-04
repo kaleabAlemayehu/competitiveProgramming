ebf97c270ae4c2eae27ffde100536d47507bab1d
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2024-09-04 16:55:05 -0400

    impliment selectionSort with golang

--- golang/main.go
+++ golang/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	// array := []int{1, 27, 2, 83, 29, 293, 32, 328, 2, 23, 232, 78293, 29}
-	array := []int{8, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	fmt.Print(*(sort.BubbleSort(array)))
+	array := []int{1, 27, 2, 83, 29, 293, 32, 328, 2, 23, 232, 78293, 29}
+	// array := []int{8, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	fmt.Print(*(sort.SelectionSort(array)))
 }
---
+++ golang/sort/selectionSort.go
@@ -0,0 +1,18 @@
+package sort
+
+func SelectionSort(arr []int) *[]int {
+	for i := 0; i < len(arr)-1; i++ {
+		min := i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[min] > arr[j] {
+				min = j
+			}
+		}
+		if i != min {
+			temp := arr[i]
+			arr[i] = arr[min]
+			arr[min] = temp
+		}
+	}
+	return &arr
+}
