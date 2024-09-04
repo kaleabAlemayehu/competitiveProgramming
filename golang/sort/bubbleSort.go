fc636c4a3514a106e832017af2d0525e3c9ac389
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2024-09-04 16:14:40 -0400

    add optimazation on bubble sort to prevent unnessessary looping after no swap

--- golang/sort/bubbleSort.go
+++ golang/sort/bubbleSort.go
@@ -3,12 +3,18 @@ package sort
 func BubbleSort(arr []int) *[]int {
 
 	for i := len(arr) - 1; i > 0; i-- {
+		noSwap := true
+
 		for j := 0; j < i; j++ {
 			if arr[j] > arr[j+1] {
 				temp := arr[j]
 				arr[j] = arr[j+1]
 				arr[j+1] = temp
+				noSwap = false
 			}
+		}
+		if noSwap {
+			break
 		}
 	}
 
