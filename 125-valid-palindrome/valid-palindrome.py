f8624748b9a2eaf9363e9ac455336d89d1568cbf
Kaleab Alemayehu <116624166+kaleabAlemayehu@users.noreply.github.com>
2025-05-17 18:57:05 -0400

    Time: 15 ms (63.71%) | Memory: 12.7 MB (90.10%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -8,19 +8,15 @@ class Solution(object):
         i = 0
         j = len(s) - 1
 
-        while i <= j:
-            if not s[i].isalnum():
+        while i < j:
+            while i < j and not s[i].isalnum():
                 i+=1
-                continue
-            if not s[j].isalnum():
+            while i < j and not s[j].isalnum():
                 j-=1
-                continue
             if s[i] != s[j]:
                 return False
-            else:
-                i+=1
-                j-=1
-                continue
+            i+=1
+            j-=1
         return True
 
             
