280ba0ec853c3fe49de45b96691f68e3dfeb823b
Kaleab Alemayehu <116624166+kaleabAlemayehu@users.noreply.github.com>
2025-05-18 14:45:28 -0400

    Time: 4 ms (47.68%) | Memory: 14 MB (8.25%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -4,24 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        #[1, 2, 3, 5, 2, 1, 1]
-        #          i        j
-        #[1, 1, 1, 2, 2, 3, 5]
-        i = 0
         j = 1
-        while j < len(nums):
-            if nums[i] >= nums[j]:
-                j+=1
-                continue
-            if nums[i] < nums[j]:
-                if nums[i+1] < nums[j]:
-                    i+=1
-                    nums[i], nums[j] = nums[j], nums[i]
-                    j+=1
-                else:
-                    i+=1
-                    j+=1
-                continue
-        return len(nums[0:i+1])
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i -1]:
+                nums[j] = nums[i]
+                j += 1
+        return j
             
         
