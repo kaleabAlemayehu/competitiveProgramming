fa0638220ec6c59eca345799c0d81197fe008132
Kaleab Alemayehu <116624166+kaleabAlemayehu@users.noreply.github.com>
2025-05-20 17:42:45 -0400

    Time: 12 ms (37.49%) | Memory: 12.6 MB (12.99%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -4,46 +4,15 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pair = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000,
+        roman_to_int = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
         }
         sum = 0
-        i = 0
-        while i < len(s):
-            if s[i] == "I" and i < len(s) - 1:
-                if s[i+1] == "V":
-                    sum += 4
-                    i+=2
-                    continue
-                if s[i+1]  == "X":
-                    sum +=9
-                    i+=2
-                    continue
-            if s[i] == "X" and i < len(s) - 1:
-                if s[i+1] == "L":
-                    sum += 40
-                    i+=2
-                    continue
-                if s[i+1] == "C":
-                    sum += 90
-                    i+=2
-                    continue
-            if s[i] == "C" and i < len(s) - 1:
-                if s[i+1] == "D":
-                    sum += 400
-                    i+=2
-                    continue
-                if s[i+1] == "M":
-                    sum += 900
-                    i+=2
-                    continue
-            sum += pair[s[i]]
-            i+=1
-        return  sum
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman_to_int[s[i]] < roman_to_int[s[i+1]]:
+                sum -= roman_to_int[s[i]]
+            else:
+                sum += roman_to_int[s[i]]
+        return sum
             
