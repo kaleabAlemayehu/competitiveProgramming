2932047d315a7675fc389c1d663ec79350bea18b
Kaleab Alemayehu <116624166+kaleabAlemayehu@users.noreply.github.com>
2025-05-25 19:13:05 -0400

    Time: 8 ms (67.18%) | Memory: 15.8 MB (83.40%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -2,6 +2,7 @@ class MinStack(object):
 
     def __init__(self):
         self.stack = []
+        self.minstack = []
 
         
 
@@ -11,31 +12,35 @@ class MinStack(object):
         :rtype: None
         """
         if len(self.stack) == 0:
-            self.stack.append([val, val])
-        elif val < self.stack[len(self.stack)-1][1]:
-            self.stack.append([val, val])
+            self.stack.append(val)
+            self.minstack.append(val)
+        elif val < self.minstack[len(self.minstack)-1]:
+            self.stack.append( val)
+            self.minstack.append(val)
         else:
-            self.stack.append([val, self.stack[len(self.stack)-1][1]])
+            self.stack.append(val)
+            self.minstack.append(self.minstack[len(self.minstack)-1])
 
     def pop(self):
         """
         :rtype: None
         """
         self.stack.pop()
+        self.minstack.pop()
         
 
     def top(self):
         """
         :rtype: int
         """
-        return self.stack[len(self.stack) - 1][0]
+        return self.stack[len(self.stack) - 1]
         
 
     def getMin(self):
         """
         :rtype: int
         """
-        return self.stack[len(self.stack) - 1][1]
+        return self.minstack[len(self.minstack) - 1]
         
 
 
