e3461191b3e16a7d00c62a5b2cd629b22eac424c
Kaleab Alemayehu <116624166+kaleabAlemayehu@users.noreply.github.com>
2025-05-27 21:39:42 -0400

    Time: 6 ms (29.09%) | Memory: 17.9 MB (57.99%) - LeetSync

--- 2159-two-out-of-three/two-out-of-three.py
+++ 2159-two-out-of-three/two-out-of-three.py
@@ -1,14 +1,11 @@
 class Solution:
     def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
         hm = {}
-        nums1 =set(nums1) 
-        nums2 =set(nums2) 
-        nums3 =set(nums3) 
-        for n in nums1:
+        for n in set(nums1):
             hm[n] = hm.get(n,0) + 1
-        for n in nums2:
+        for n in set(nums2):
             hm[n] = hm.get(n,0) + 1
-        for n in nums3:
+        for n in set(nums3):
             hm[n] = hm.get(n,0) + 1
         resSet =set() 
         for key, val in hm.items():
