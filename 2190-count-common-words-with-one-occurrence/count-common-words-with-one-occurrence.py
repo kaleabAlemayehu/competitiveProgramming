a1573b51e78bd093009d7f6b2756c16aefdfd6a9
Kaleab Alemayehu <116624166+kaleabAlemayehu@users.noreply.github.com>
2025-05-30 20:03:38 -0400

    Time: 0 ms (100.00%) | Memory: 18.1 MB (70.96%) - LeetSync

--- 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.py
+++ 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.py
@@ -5,7 +5,8 @@ class Solution:
         for s in words1:
             fq1[s] = fq1.get(s, 0) + 1
         for s in words2:
-            fq2[s] = fq2.get(s, 0) + 1
+            if fq1.get(s, 0) == 1:
+                fq2[s] = fq2.get(s, 0) + 1
         output = 0 
         for key, val in fq1.items():
             if key in fq2 and fq2[key] == 1 and fq1[key] == 1:
