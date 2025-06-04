b00db377298ee886ca3769fa3e020cfa9d1742e0
Kaleab Alemayehu <116624166+kaleabAlemayehu@users.noreply.github.com>
2025-06-04 17:32:42 -0400

    Time: 7 ms (57.86%) | Memory: 19.5 MB (19.60%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,8 +1,5 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         fq = Counter(nums) 
-        nover2 = len(nums)/2
-        for key, val in fq.items():
-            if val > nover2:
-                return key
+        return max(fq, key = fq.get)
 
