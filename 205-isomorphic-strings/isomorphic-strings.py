e4ab880f5ecbd1240fece6999036f92dbb1c75a1
Kaleab Alemayehu <116624166+kaleabAlemayehu@users.noreply.github.com>
2025-06-09 18:51:31 -0400

    Time: 4 ms (73.40%) | Memory: 17.8 MB (79.60%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,14 +1,13 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        ics= defaultdict(list)
+        pair = {}
         for i, c in enumerate(s):
-            ics[c].append(i)
-        ict= defaultdict(list)
-        for i, c in enumerate(t):
-            ict[c].append(i)
-        for i, c in enumerate(s):
-            if ics[c] != ict[t[i]]:
-                return False
+            if c not in pair:
+                if t[i] in pair.values():
+                    return False
+                pair[c] = t[i]
+            else:
+                if pair[c] != t[i]:
+                    return False
         return True
-
-        
+            
