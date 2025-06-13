3d0829cd4f7a36067b4d60551a5fb1f51f63c5df
Kaleab Alemayehu <116624166+kaleabAlemayehu@users.noreply.github.com>
2025-06-13 18:21:57 -0400

    Time: 0 ms (100.00%) | Memory: 18.3 MB (45.11%) - LeetSync

--- 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
@@ -1,16 +1,19 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
-        fq1 = defaultdict(int)
-        for i, v in enumerate(list1):
-            fq1[v] = i
-        fq2 = defaultdict(int)
-        for i, v in enumerate(list2):
-            if v in fq1:
-                fq2[v] = fq1[v] + i
-        minimum = min(fq2.values())
         res = []
-        for key, val in enumerate(fq2):
-            if fq2[val] == minimum:
-                res.append(val)
+        m = float(inf)
+        print(m)
+        iMap1 = defaultdict(int)
+        for i, v in enumerate(list1):
+            iMap1[v] = i
+        iMap2 = defaultdict(int)
+        for i, v in enumerate(list2):
+            if v in iMap1 and i + iMap1[v] <= m:
+                if i + iMap1[v] == m:
+                    res.append(v)
+                    continue
+                res = [v]
+                m = i + iMap1[v]
         return res
+    
         
