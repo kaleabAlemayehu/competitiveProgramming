56f8bbed70767485c1bf5471e2af125ff510ddae
Kaleab Alemayehu <116624166+kaleabAlemayehu@users.noreply.github.com>
2025-07-11 04:16:34 -0400

    Time: 19 ms (66.39%) | Memory: 17.7 MB (87.59%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,16 +1,36 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        i, j = 0, len(s1) - 1
-        while j < len(s2):
-            count = Counter(s1)
-            for p in range(i, j+1):
-                if s2[p] in count and count[s2[p]] > 0:
-                    count[s2[p]] -= 1
-                    if p == j:
-                        return True
-                    continue
-                else:
-                    break
-            i+=1
-            j+=1
-        return False
+        if len(s1) > len(s2):
+            return False
+
+        s1Count, s2Count = [0] * 26, [0] * 26
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
+
+        matches = 0
+        for i in range(26):
+            if s1Count[i] == s2Count[i]:
+                matches += 1
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
+            i = ord(s2[r]) - ord('a')
+            s2Count[i] += 1
+            if s2Count[i] == s1Count[i]:
+                matches +=1
+            elif s2Count[i] == s1Count[i] + 1:
+                matches -=1
+
+            i = ord(s2[l]) - ord('a')
+            s2Count[i] -= 1
+            if s2Count[i] == s1Count[i]:
+                matches += 1
+            elif s2Count[i] + 1 == s1Count[i]:
+                matches -= 1
+            l+=1
+
+        return matches == 26
+
+            
